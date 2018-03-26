2f5bf932d4b891043a2a3f86e5b1a2c7fa4b5472
Nebeschenko Igor <nebeschenkoichor@gmail.com>
2018-03-26 09:55:28 +0300

    first wiev img

--- src/container/Slider.js
+++ src/container/Slider.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Slider extends Component {
+  state = {
+    sliderCount: 1
+  }
   render() {
     const { sliderItem } = this.props
+    const sliderImg = sliderItem.map((item) => 
+      <img
+        key={item.id}
+        src={item.src}
+        alt={item.title}
+      />
+    )
     return(
       <div className="slider">
-        {sliderItem}
+        {sliderImg}
       </div>
     )
   }
 }
 
-Slider.PropTypes = {
-  sliderItem: PropTypes.object.isRequired
+Slider.propTypes = {
+  sliderItem: PropTypes.array.isRequired
 }
 
 export default Slider
