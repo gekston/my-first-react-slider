a17a1d63760602463ce6cf11169eda99b527809c
Nebeschenko Igor <nebeschenkoichor@gmail.com>
2018-03-26 09:29:11 +0300

    src file

---
+++ src/assets/fixtures.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: '1',
+    title: 'first img',
+    src: 'https://images7.alphacoders.com/411/thumb-1920-411820.jpg'
+  },
+  {
+    id: '2',
+    title: 'second img',
+    src: 'http://www.guoguiyan.com/data/out/97/68939726-hobbit-wallpapers.jpg'
+  },
+  {
+    id: '3',
+    title: 'three img',
+    src: 'http://www.hdwallpapersz.in/wp-content/uploads/2016/09/butterfly-hd-img.jpg'
+  }
+]
